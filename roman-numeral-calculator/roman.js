4110f5dec6148c3b08c306b339745a0cb3418200
Maciej Kus <maciej.kus.925@gmail.com>
2016-02-12 15:44:57 -0800

    Site updated at 2016-02-12 23:44:56 UTC

---
+++ roman-numeral-calculator/roman.js
@@ -0,0 +1,22 @@
+var num  = document.getElementById('num');
+num.onchange = function() {
+  var value = this.value;
+  //remove nondigits
+  value = value.replace(/\D/g, "");
+  document.getElemebtById('roman').innerHTML = toRoman(value);
+}
+
+function toRoman(n) {
+  var rA = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+  var dA = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+
+  var ans = '';
+  for( var x = 0, y = rA.length; x < y; x++) {
+    while(n >= dA[x]) {
+      n -= dA[x];
+      ans = ans + rA[x];
+    }
+  }
+  return ans;
+}
+
